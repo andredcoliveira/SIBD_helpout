a9c6309c441391670a48dbfff40e1d0d1020fdcb
André Duarte <up201405639@fe.up.pt>
2017-11-26 19:50:06 +0000

    changed queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,10 +1,13 @@
--- Pedidos em que user é dono
+-- Pedidos em que user é dono (substituir 1 pelo id)
 
 SELECT *
 FROM users JOIN users_pedido ON (users.id = users_id)
+WHERE users.id = 1
+
 
 
 -- Pedidos em que user está a ajudar
+
 
 
 -- Inserir Pedido
@@ -14,3 +17,7 @@ VALUES (DEFAULT, 'nome', 'recompensa', CURRENT_TIMESTAMP, 'descrição', 'local'
 RETURNING id
 
 INSERT INTO users_pedido VALUES ($_ID, ?, 'true')
+
+
+
+-- Buscar pedido por ID (a partir do $_GET que traz ID do URL)
