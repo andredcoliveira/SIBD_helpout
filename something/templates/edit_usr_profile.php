7037b3fa6621ea6096ac425b6d6d5b034fb50c49
danielgranhao <up201406280@fe.up.pt>
2017-11-27 21:16:25 +0000

    working on edit profile, not ready

--- something/templates/edit_usr_profile.php
+++ something/templates/edit_usr_profile.php
@@ -4,20 +4,21 @@
     <div class="new_request">
       <h2>Editar Perfil</h2>
 
-      <form action="actions/new_request.php" method="post" enctype="multipart/form-data">
-        Nome:<input type="text" name="name" value="Daniel Granhão" required="required">
-        Password:<input type="password" name="pw" required="required">
-        Repita a Password:<input type="password" name="pw" required="required">
-        Data de nascimento:<input type="date" name="date" value="1996-06-03">
-        Descrição:<textarea rows="4" cols="64">Não me apetece.. </textarea>
+      <form action="actions/action_edit_profile.php" method="post" enctype="multipart/form-data">
+        <label for="name"><h3 class="title">Nome:</h3></label><input type="text" name="name" value="Daniel Granhão" required="required">
+        <label for="pw"><h3 class="title">Password:</h3></label><input type="password" name="pw" required="required">
+        <label for="pw2"><h3 class="title">Repita a Password:</h3></label><input type="password" name="pw2" required="required">
+        <label for="date"><h3 class="title">Data de nascimento:</h3></label><input type="date" name="date" value="1996-06-03">
+        <label for="description"><h3 class="title">Descrição:</h3></label><textarea rows="4" cols="64" name="description" id="edit_profile_description">Não me apetece.. </textarea>
 
         <label>Escolha uma foto de perfil:
           <input type="file" name="image">
         </label>
-        <input type="submit" value="Submeter">
+       
+        <input type="submit" value="Submeter" name="submit" id="submit_button_edit_profile">
       </form>
 
-      <form action="delete_account.php" method="post" id="delete_form">
+      <form action="actions/action_delete_account.php" method="post" id="delete_form">
         <input type="submit" value="Desativar Conta">
         <p>Desativar a sua conta fará com que ela deixe de estar visível até à proxima vez que efetuar login.</p>
       </form>
