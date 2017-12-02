9b0453a45b60db9eea6294b42107d84590f0a45a
danielgranhao <up201406280@fe.up.pt>
2017-12-02 16:44:57 +0000

    Request gets deleted when upload fails

--- something/actions/action_new_request.php
+++ something/actions/action_new_request.php
@@ -34,7 +34,11 @@
     die(header('Location: ../new_request.php'));
 	}
 
+	$uploadOk = true;
+
 	if(!isset($_FILES['fileToUpload']) || $_FILES['fileToUpload']['error'] == UPLOAD_ERR_NO_FILE) {
+		$_SESSION['success_message'] = 'Pedido sem imagem criado com sucesso.';
+		die(header("Location: ../request.php?id=$request_id"));
 		/** DO NOTHING **/
 	} else {
 		$extension = explode('.' , basename($_FILES["fileToUpload"]["name"]));
@@ -46,48 +50,52 @@
 		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
 	    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	    if(!($check !== false)) {
-				$_SESSION['error_message'] = "O ficheiro tem de ser uma imagem.";
-				die(header('Location: ../new_request.php'));
-	    }
+		    if(!($check !== false)) {
+					$_SESSION['error_message'] = "O ficheiro tem de ser uma imagem.";
+					$uploadOk = false;
+		    }
 		}
 
 		// Check if file already exists
 		$possibleImagePath = getRequestPhoto2($request_id);
 		if (file_exists($possibleImagePath)) {
-      $status = unlink($possibleImagePath); //remove the file
-      if($status != true){
-        //$_SESSION['error_message'] = $possibleImagePath . '-> não consegue apagar isto';
-        $_SESSION['error_message'] = 'Ocorreu um problema ao eliminar a imagem atual.';
-				die(header('Location: ../new_request.php'));
-      }
-    }
+	      $status = unlink($possibleImagePath); //remove the file
+	      if($status != true){
+	        //$_SESSION['error_message'] = $possibleImagePath . '-> não consegue apagar isto';
+	        $_SESSION['error_message'] = 'Ocorreu um problema ao eliminar a imagem atual.';
+	        $uploadOk = false;
+	      }
+	    }
 
 		// Check file size
 		if ($_FILES["fileToUpload"]["size"] > 2000000) {
-      //echo "Sorry, your file is too large.";
-      $_SESSION['error_message'] = "A imagem não pode exceder 2MB";
-			die(header('Location: ../new_request.php'));
+			//echo "Sorry, your file is too large.";
+			$_SESSION['error_message'] = "A imagem não pode exceder 2MB";
+			$uploadOk = false;
 		}
 
     // Allow certain file formats - necessário? já foi verificado atrás se é imagem, podemos permitir mais tipos
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-      //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-      $_SESSION['error_message'] = "A imagem tem de ser JPG, JPEG, PNG ou GIF.";
-			die(header('Location: ../new_request.php'));
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+     		 //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      		$_SESSION['error_message'] = "A imagem tem de ser JPG, JPEG, PNG ou GIF.";
+      		$uploadOk = false;
 		}
 
 
 
 		// if everything is ok, try to upload file
-	  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	    //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-	    $_SESSION['success_message'] = 'Pedido criado com sucesso.';
-	  } else {
-			$_SESSION['error_message'] = 'Houve um problema com o carregamento da imagem.';
+		if($uploadOk){
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			//echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			$_SESSION['success_message'] = 'Pedido criado com sucesso.';
+			} else {
+				$_SESSION['error_message'] = 'Houve um problema com o carregamento da imagem.';
+				$uploadOk = false;
+			}
+		} else {
+			deleteRequest($request_id);
 			die(header('Location: ../new_request.php'));
-	  }
+		}
 	}
 
 	header("Location: ../request.php?id=$request_id");
