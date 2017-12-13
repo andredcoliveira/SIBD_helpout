12b4f456fdb2f65f10bdd589ed5aa092bd58a822
André Duarte <up201405639@fe.up.pt>
2017-12-13 09:00:12 +0000

    edit request - permission

--- something/edit_request.php
+++ something/edit_request.php
@@ -10,9 +10,13 @@
   if(!isset($_USERNAME)) {
     die(header('Location: index.php'));
   }
+  if(getRequestOwner($_CURR_REQ)['id'] != $_ID) {
+    die(header('Location: request.php?id=' . $_CURR_REQ));
+  }
 
   $skills = getAllSkills();
 
+  $_SESSION['current_request'] = $_CURR_REQ;
   $request = getRequest($_CURR_REQ);
 
   include('templates/header.php');
