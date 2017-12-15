c4b75c7f837110671028c53fae87ee24b6ae23da
André Duarte <up201405639@fe.up.pt>
2017-12-15 19:54:19 +0000

    final before submit

--- something/tools/request.php
+++ something/tools/request.php
@@ -1,7 +1,7 @@
 <?php
 
   function getRequestPhoto($request_id) {
-    if($request_id == false) return false;
+    if($request_id === false) return false;
     $pattern = "res/uploads/requests/" . $request_id . ".*";
     if(isset(glob($pattern)[0])) {
       return glob($pattern)[0];
@@ -10,7 +10,7 @@
   }
 
   function getRequestPhoto2($request_id) {
-    if($request_id == false) return false;
+    if($request_id === false) return false;
     $pattern = "../res/uploads/requests/" . $request_id . ".*";
     if(isset(glob($pattern)[0])) {
       return glob($pattern)[0];
